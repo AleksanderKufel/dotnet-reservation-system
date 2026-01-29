4714d8a925b437adec486c3fcfbfc0d283251361
AleksanderKufel <31322094+AleksanderKufel@users.noreply.github.com>
2026-01-29 23:15:34 +0100

    Add application service for creating reservations

---
+++ ReservationSystem/ReservationSystem.Application/Interfaces/IReservationRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReservationSystem.Domain.Entities;
+
+namespace ReservationSystem.Application.Interfaces;
+
+public interface IReservationRepository
+{
+    Task<IReadOnlyList<Reservation>> GetActiveForSpecialistAsync(
+        Guid specialistId,
+        DateTime from,
+        DateTime to);
+
+    Task AddAsync(Reservation reservation);
+}
---
+++ ReservationSystem/ReservationSystem.Application/Services/ReservationService.cs
@@ -0,0 +1,42 @@
+﻿using ReservationSystem.Application.Interfaces;
+using ReservationSystem.Domain.Entities;
+using ReservationSystem.Domain.Services;
+
+namespace ReservationSystem.Application.Services;
+
+public class ReservationService
+{
+    private readonly IReservationRepository _repository;
+    private readonly ReservationConflictChecker _conflictChecker;
+
+    public ReservationService(
+        IReservationRepository repository,
+        ReservationConflictChecker conflictChecker)
+    {
+        _repository = repository;
+        _conflictChecker = conflictChecker;
+    }
+
+    public async Task CreateReservationAsync(
+        Guid specialistId,
+        Guid userId,
+        DateTime startTime,
+        DateTime endTime)
+    {
+        var existingReservations = await _repository.GetActiveForSpecialistAsync(
+            specialistId,
+            startTime,
+            endTime);
+
+        var reservation = new Reservation(
+            specialistId,
+            userId,
+            startTime,
+            endTime);
+
+        if (_conflictChecker.HasConflict(reservation, existingReservations))
+            throw new InvalidOperationException("Reservation time conflict.");
+
+        await _repository.AddAsync(reservation);
+    }
+}
